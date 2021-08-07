7e52d245f595a053d179120152b28399e443e0c6
Vinicius Sales <vinicius.2010.s@gmail.com>
2021-08-07 11:43:07 -0300

    omgsosorry

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,38 @@
-use std::path::Path;
 use string_cache::Atom;
+use std::path::Path;
 use swc_common::{self, sync::Lrc, SourceMap};
-use swc_ecma_ast::{ExprStmt, MemberExpr};
-use swc_ecma_parser::{lexer::{Lexer}, Capturing, Parser, StringInput, Syntax};
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput};
+use swc_ecma_ast::*;
 
 fn main() {
     let cm: Lrc<SourceMap> = Default::default();
+
     let fm = cm
         .load_file(Path::new("foo.js"))
-        .expect("failed to load foo.js");
+        .expect("faile to load foo.js");
 
     let lexer = Lexer::new(
-        Syntax::default(),
+        Default::default(),
         Default::default(),
         StringInput::from(&*fm),
         None,
     );
 
-    let capturing = Capturing::new(lexer);
-    let mut parser = Parser::new_from(capturing);
+    let mut parser = Parser::new_from(lexer);
 
-    let script = parser.parse_script().unwrap();
+    let script = parser.parse_script().expect("Failed to parse script");
     for stmt in script.body {
         if stmt.is_expr() {
             let expr = stmt.expr().unwrap();
-            if is_console_log(expr) {
-                println!("found console log!")
+            if is_console_log(&expr) {
+                println!("found console log")
             }
         }
     }
 }
 
-fn is_console_log(expr_stmt: ExprStmt) -> bool {
-    let expr = *expr_stmt.expr;
-
-    match expr.call() {
+fn is_console_log(expr_stmt: &ExprStmt) -> bool {
+    match expr_stmt.clone().expr.call() {
         Some(call) => match call.callee.expr() {
             Some(expr) => match expr.member() {
                 Some(member) => is_console_obj_expr(&member) && is_log_prop_expr(&member),
